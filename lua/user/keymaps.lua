e32b49e8c5eaba64a0501eee3d898963e3b9c618
mateoruiz5171 <mateoruiz5171@gmail.com>
2022-11-01 18:51:18 -0500

    keymappings and options

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,65 @@
+local opts = { noremap = true, silent = true}
+
+local term_opts = {silent = true}
+
+local keymap = vim.api.nvim_set_keymap
+
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Normal Mode --
+
+-- window nav
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- resize windows 
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- move quickly 
+keymap("n", "<Up>", "10k", opts)
+keymap("n", "<Down>", "10j", opts)
+keymap("n", "<Left>", "10h", opts)
+keymap("n", "<Right>", "10l", opts)
+
+-- buffer nav
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- split screen
+keymap("n", "|", ":vsplit<CR>", opts)
+keymap("n", "-", ":vsplit<CR>", opts)
+
+-- save shortcut
+keymap("n", "<C-s>", ":w<cr>", opts)
+keymap("n", "<C-z>", ":u<cr>", opts)
+
+-- Insert Mode --
+
+-- exit insert mode  
+keymap("i", "jk", "<ESC>", opts)
+
+-- Visual Mode --
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
